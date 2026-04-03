497da405e145ad0b426dc16edddf91e9d2eacd71
Glen <59471231+GHarding95@users.noreply.github.com>
2026-04-03 17:11:08 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 import './App.css';
 import RoverStatus from './components/RoverStatus';
 import CommandPanel from './components/CommandPanel';
