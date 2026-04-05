50071335eead30a3569301bb3f8cd6ea34348af9
Glen <59471231+GHarding95@users.noreply.github.com>
2026-04-05 05:06:23 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './fonts-inter-latin.css'
+import './fonts-mono.css'
 import './index.css'
 import App from './App.tsx'
-
-void import('./fonts-mono.css')
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
